95491238e1af221852332e97e7d3393501113cd2
zhangxg <xiaoguang.zhang@capitalonline.net>
2020-10-15 16:47:48 +0800

    实现采集mongo慢日志

--- mongo/mongo.go
+++ mongo/mongo.go
@@ -2,14 +2,9 @@ package mongo
 
 import (
 	"context"
-	"fmt"
-	"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/bson/primitive"
-	"log"
-	"time"
-
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
+	"log"
 )
 
 // NewConnection
@@ -25,38 +20,5 @@ func NewConnection(urls string) (*mongo.Client, error) {
 
 
 	return client, nil
-}
-
-func Auto(client *mongo.Client, after primitive.Timestamp,db string,ctx context.Context){
-	ticker := time.NewTicker(10 * time.Second)
-	defer func() {
-		ticker.Stop()
-	}()
-
-	for {
-		select {
-		case <-ticker.C:
-			GetOpLogCursor(client,after,db,ctx)
-		}
-	}
-}
-func GetOpLogCursor(client *mongo.Client, after primitive.Timestamp,db string,ctx context.Context) {
-	filter := bson.D{{"ts", bson.M{"$gt": after}}}
-	var doc SystemProfile
-	opts := &options.FindOptions{}
-	opts.SetCursorType(options.TailableAwait)
-	fmt.Println("system.profile start.... ",db)
-	cursor, err := client.Database(db).Collection("system.profile").Find(ctx,filter,opts)
-	if err!=nil{
-		panic(err)
-	}
-	for cursor.Next(ctx){
-		err := cursor.Decode(&doc)
-		if err!=nil{
-			return
-		}
-		fmt.Println("system.profile is ",doc)
-	}
-
 }
 
